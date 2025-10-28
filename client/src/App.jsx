b3708369be558f8d58edb36ae1d10e42e85877cd
lynx-fx <anupbhujel07@gmail.com>
2025-10-28 17:33:23 +0545

    Added 404 and loading page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
+import Landing from "./component/landing.jsx";
+import Login from "./component/login.jsx";
+import NotFound from "./component/notFound.jsx";
+import Dashboard from "./component/dashboard.jsx";
 
 function App() {
   return (
-    <div>
-      <h1 className='mx-10'>Tailwind configuration testing</h1>
-    </div>
-  )
+    <>
+      <Toaster richColors />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
