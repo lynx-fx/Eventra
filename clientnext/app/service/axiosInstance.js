e0c38617dd7db92f10be13900f2821d0e4e7ff3c
lynx-fx <anupbhujel07@gmail.com>
2025-11-08 20:07:18 +0545

    Partial next migration

---
+++ clientnext/app/service/axiosInstance.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const BACKEND = import.meta.env.PROD
+  ? import.meta.env.VITE_BACKEND_HOSTED
+  : import.meta.env.VITE_BACKEND_LOCAL;
+
+const axiosInstance = axios.create({
+  baseURL: BACKEND,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  validateStatus: () => true,
+});
+
+export default axiosInstance;
